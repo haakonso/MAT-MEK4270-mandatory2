Complete. (Cosine: Spotted wrong argument. Neumann<Legendre/Chebyshev>: updated stiffness matrix S to reflect scaling factor in basis functions.)

galerkin.py
@@ -236,25 +236,28 @@ class Sines(Trigonometric):
 class Cosines(Trigonometric):
     def __init__(self, N, domain=(0, 1), bc=(0, 0)):
         Trigonometric.__init__(self, N, domain=domain)
-        self.B = Dirichlet(bc, domain, self.reference_domain)
+        self.B = Neumann(bc, domain, self.reference_domain)
         #raise NotImplementedError
 
     def basis_function(self, j, sympy=False):
         if sympy:
-            return sp.cos((j + 1) * sp.pi * x)
-        return lambda Xj: np.cos((j + 1) * np.pi * Xj)
+            return sp.cos(j * sp.pi * x)
+        return lambda Xj: np.cos(j * np.pi * Xj)
         #raise NotImplementedError
 
     def derivative_basis_function(self, j, k=1):
-        scale = ((j + 1) * np.pi) ** k * {0: 1, 1: -1}[(k // 2) % 2]
+        scale = (j * np.pi) ** k * {0: 1, 1: -1}[(k // 2) % 2]
         if k % 2 == 0:
-            return lambda Xj: scale * np.cos((j+1) * np.pi * Xj)
+            return lambda Xj: scale * np.cos(j * np.pi * Xj)
         else:
-            return lambda Xj: scale * np.sin((j+1) * np.pi * Xj)
+            return lambda Xj: scale * np.sin(j * np.pi * Xj)
         #raise NotImplementedError
 
     def L2_norm_sq(self, N):
-        return 0.5
+        arr = np.zeros(N+1)
+        arr[0] = 1.0
+        arr[1:] = 0.5
+        return arr
         #raise NotImplementedError
 
 
@@ -352,14 +355,18 @@ class NeumannLegendre(Composite, Legendre):
     def __init__(self, N, domain=(-1, 1), bc=(0, 0), constraint=0):
         Legendre.__init__(self, N, domain=domain)
         self.B = Neumann(bc, domain, self.reference_domain)
-        self.S = sparse.diags((1, -1), (0, 2), shape=(N + 1, N + 3), format="csr")
+        self.scale = lambda j: (j*(j+1)) / ((j+2)*(j+3))
+        self.scale_arr = np.array([self.scale(j) for j in range(N+1)])
+        self.S = sparse.diags(diagonals=( 1,  -self.scale_arr),
+                              offsets=(0, 2),
+                              shape=(N+1, N+3),
+                              format="csr")
         #raise NotImplementedError
 
     def basis_function(self, j, sympy=False):
-        scale = (j*(j+1)) / ((j+2)*(j+3))
         if sympy:
-            return sp.legendre(j,x) - scale * sp.legendre(j+2, x)
-        return Leg.basis(j) - scale * Leg.basis(j+2)
+            return sp.legendre(j,x) - self.scale(j) * sp.legendre(j+2, x)
+        return Leg.basis(j) - self.scale_arr[j] * Leg.basis(j+2)
         #raise NotImplementedError
 
 
@@ -379,18 +386,18 @@ class NeumannChebyshev(Composite, Chebyshev):
     def __init__(self, N, domain=(-1, 1), bc=(0, 0), constraint=0):
         Chebyshev.__init__(self, N, domain=domain)
         self.B = Neumann(bc, domain, self.reference_domain)
-        self.S = sparse.diags((1, -1), (0, 2), shape=(N + 1, N + 3), format="csr")
+        self.scale = lambda j: (j**2) / ((j+2)**2)
+        self.scale_arr = np.array([self.scale(j) for j in range(N+1)])
+        self.S = sparse.diags(diagonals=(1,  -self.scale_arr),
+                              offsets=(0, 2),
+                              shape=(N+1, N+3),
+                              format="csr")
         #raise NotImplementedError
 
     def basis_function(self, j, sympy=False):
-        scale = (j**2) / ((j+2)**2)
-        #Qj = Cheb.basis(j)
-        #Qj2 = Cheb.basis(j + 2)
-        #scale = Qj.deriv()(1) / Qj2.deriv()(1)
         if sympy:
-            return sp.cos(j * sp.acos(x)) - scale * sp.cos((j + 2) * sp.acos(x))
-        return Cheb.basis(j) - scale * Cheb.basis(j + 2)
-        #return Qj - scale * Qj2
+            return sp.cos(j * sp.acos(x)) - self.scale(j) * sp.cos((j + 2) * sp.acos(x))
+        return Cheb.basis(j) - self.scale_arr[j] * Cheb.basis(j + 2)
         #raise NotImplementedError
 
 
@@ -499,9 +506,9 @@ def test_helmholtz():
     for space in (
         NeumannChebyshev,
         NeumannLegendre,
-        #DirichletChebyshev,
-        #DirichletLegendre,
-        #Sines,
+        DirichletChebyshev,
+        DirichletLegendre,
+        Sines,
         Cosines,
     ):
         if space in (NeumannChebyshev, NeumannLegendre, Cosines):
@@ -541,6 +548,6 @@ def test_convection_diffusion():
 
 
 if __name__ == "__main__":
-    #test_project()
-    #test_convection_diffusion()
+    test_project()
+    test_convection_diffusion()
     test_helmholtz()